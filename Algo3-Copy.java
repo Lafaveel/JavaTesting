90c1f54fa8ae6661f8f2b9077d6ced2c474cd6dc
Ethan LaFave <lafavejl@live.com>
2017-11-29 23:17:12 -0500

    Added copy 3 and modified Algo2

---
+++ Algo3-Copy.java
@@ -0,0 +1,36 @@
+public class Algo3 {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+
+  }
+
+  public int orderTotal(Item[] cart) {
+    return 5;
+  }
+}
+
+class Item {
+  private String description;
+  private int price;
+
+  public Item(String d, int p) {
+    this.description = d;
+    this.price = p;
+  }
+
+  public void setDescription(String d) {
+    this.description = d;
+  }
+
+  public void setPrice(String d) {
+    this.description = d;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public int getPrice(int p) {
+    return this.p;
+  }
+}
